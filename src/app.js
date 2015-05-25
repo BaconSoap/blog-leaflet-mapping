2c752c18c82a99df2c2bf758c3e7cdb3e3bc27ca
Andrew Varnerin <andrew@varnerin.info>
2015-05-25 15:20:05 -0400

    encapsulate

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-// create a new map with no base layer
-var map = new L.Map("map", {
-  center: new L.LatLng(42.3964631, -71.1205171),
-  zoom: 16
-});
+(function(){
+  var map;
 
-// use Stamen's 'terrain' base layer
-var layer = new L.StamenTileLayer("terrain");
-map.addLayer(layer);
+  function init() {
+    // create a new map with no base layer
+    var map = new L.Map("map", {
+      center: new L.LatLng(42.3964631, -71.1205171),
+      zoom: 16
+    });
+
+    // use Stamen's 'terrain' base layer
+    var layer = new L.StamenTileLayer("terrain");
+    map.addLayer(layer);
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+})();
