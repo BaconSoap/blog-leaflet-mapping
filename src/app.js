4fc20fc5793023912cb75e8e74b83227ed8957bc
Andrew Varnerin <andrew@varnerin.info>
2015-06-13 19:37:51 -0400

    color contained points

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@
   var nextMarkerNumber = 1;
   var catImg = '<img src="http://thecatapi.com/api/images/get?format=src&type=png&size=small">';
   var isDrawing = false;
+  var polygons = {};
+  var nextPolygonId = 0;
+  var markers = [];
+  var colors = ['green', 'red', 'blue', 'orange', 'black', 'yellow'];
 
   function init() {
     // create a new map with no base layer
@@ -26,6 +30,8 @@
 
     map.on('editable:drawing:start', onDrawingStart);
     map.on('editable:drawing:end', onDrawingEnd);
+    map.on('editable:vertex:dragend', assignContainedPoints);
+    map.on('editable:drawing:click', assignContainedPoints);
   }
 
   /**
@@ -35,6 +41,12 @@
     var marker = L.marker(coords);
     marker.bindPopup('<h3>I\'m #' + nextMarkerNumber++ + '</h3>' + catImg);
     marker.addTo(map);
+    markers.push(marker);
+    for (var k in polygons) {
+      if (polygons.hasOwnProperty(k)) {
+        markContainedPoints([marker], polygons[k]);
+      }
+    }
   }
 
   function onClick(e) {
@@ -44,13 +56,44 @@
   function onDrawingStart(e) {
     isDrawing = true;
     util.getById('toggleDrawing').innerText = 'Stop Drawing';
-    console.log(e);
+    var polygon = e.layer;
+    polygon.polygonId = nextPolygonId++;
+    polygons[polygon.polygonId] = polygon;
+  }
+
+  function assignContainedPoints() {
+    var markersCopy = [];
+    for (var i = 0; i < markers.length; i++) {
+      markersCopy.push(markers[i]);
+    }
+    for (var k in polygons) {
+      if (polygons.hasOwnProperty(k)) {
+        markContainedPoints(markersCopy, polygons[k]); 
+      }
+    }
+    markersCopy.forEach(function(m) {
+      m._icon.style.backgroundColor = 'transparent';
+    });
+  }
+
+  function markContainedPoints(markers, poly) {
+    var id = poly.polygonId;
+    for(var i = 0; i < markers.length; i++) {
+      var m = markers[i];
+      var point = m.getLatLng();
+      point = [point.lng, point.lat];
+      if (turf.inside(turf.point(point), poly.toGeoJSON())) {
+        m.ownedBy = id;
+        m._icon.style.backgroundColor = colors[(id < colors.length? id: colors.length - 1)];
+        markers.splice(i, 1);
+        i--;
+      }
+    }
   }
 
   function onDrawingEnd(e) {
     isDrawing = false;
     util.getById('toggleDrawing').innerText = 'Draw';
-    console.log(e);
   }
 
   function toggleDrawing() {
